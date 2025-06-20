44aa23fcd01036bb94fba83da5b2792810941e46
sureshe456 <160699174+sureshe456@users.noreply.github.com>
2025-06-20 14:29:53 +0530

    Update macos.sh

--- integration-tests/macos.sh
+++ integration-tests/macos.sh
@@ -8,7 +8,7 @@ ANY_CA_SUBJECT="OU=GlobalSign Root CA - R3, O=GlobalSign, CN=GlobalSign"
 reset() {
   #sudo security remove-trusted-cert -d $ANY_CA_PEM || true
   CERT_HASH=$(openssl x509 -in $ANY_CA_PEM -noout -fingerprint -sha1 | cut -d= -f2 | tr -d ':')
-  security delete-certificate -Z "$CERT_HASH" /Library/Keychains/System.keychain
+  sudo security delete-certificate -Z "$CERT_HASH" /Library/Keychains/System.keychain
   list | grep "$ANY_CA_SUBJECT"
 } 
 
@@ -30,7 +30,7 @@ assert_exists() {
 
 test_distrust_existing_root() {
   assert_exists "$ANY_CA_SUBJECT"
-  security add-trusted-cert -d -r deny $ANY_CA_PEM
+  sudo security add-trusted-cert -d -r deny $ANY_CA_PEM
   #sudo security add-trusted-cert -k /Library/Keychains/System.keychain -d -r deny $ANY_CA_PEM
 
   assert_missing "$ANY_CA_SUBJECT"
@@ -38,10 +38,10 @@ test_distrust_existing_root() {
 }
 
 # https://developer.apple.com/forums/thread/671582?answerId=693632022#693632022
-security authorizationdb write com.apple.trust-settings.admin allow
+sudo security authorizationdb write com.apple.trust-settings.admin allow
 
 #security find-certificate -a -c "GlobalSign"
-security find-certificate -a -c "GlobalSign" /Library/Keychains/System.keychain || echo "Not found"
+sudo security find-certificate -a -c "GlobalSign" /Library/Keychains/System.keychain || echo "Not found"
 
 reset
 test_distrust_existing_root
