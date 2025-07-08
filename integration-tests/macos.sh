18e84c7515ce73287ba32019bb5f96a6959b43c9
sureshe456 <160699174+sureshe456@users.noreply.github.com>
2025-07-08 17:52:39 +0530

    Update macos.sh

--- integration-tests/macos.sh
+++ integration-tests/macos.sh
@@ -7,7 +7,8 @@ ANY_CA_SUBJECT="OU=GlobalSign Root CA - R3, O=GlobalSign, CN=GlobalSign"
 
 reset() {
   #sudo security remove-trusted-cert -d $ANY_CA_PEM || true
-  CERT_HASH=$(openssl x509 -in $ANY_CA_PEM -noout -fingerprint -sha1 | cut -d= -f2 | tr -d ':')
+  #CERT_HASH=$(openssl x509 -in $ANY_CA_PEM -noout -fingerprint -sha1 | cut -d= -f2 | tr -d ':')
+  CERT_HASH=$(sudo security find-certificate -c "GlobalSign Root CA - R3" -Z /Library/Keychains/System.keychain | grep 'SHA-1 hash:' | awk '{print $3}')
   echo "Attempting to delete cert with hash: $CERT_HASH"
   sudo security find-certificate -Z $CERT_HASH /Library/Keychains/System.keychain || echo "Cert not found"
   sudo security delete-certificate -Z $CERT_HASH /Library/Keychains/System.keychain || echo "Delete failed"
