aad32811816a488919c0edfcd59954dfb4a0f2f1
sureshe456 <160699174+sureshe456@users.noreply.github.com>
2025-07-09 16:29:38 +0530

    Update macos.sh

--- integration-tests/macos.sh
+++ integration-tests/macos.sh
@@ -20,15 +20,13 @@ ANY_CA_SUBJECT="OU=GlobalSign Root CA - R3, O=GlobalSign, CN=GlobalSign"
 #}
 
 reset() {
-  SUBJECT="OU=GlobalSign Root CA - R3, O=GlobalSign, CN=GlobalSign"
-  echo "Attempting to delete cert with subject: $SUBJECT"
+  #SUBJECT="OU=GlobalSign Root CA - R3, O=GlobalSign, CN=GlobalSign"
+  #echo "Attempting to delete cert with subject: $SUBJECT"
   
   # Find SHA-1 hash by subject SHA-1 hash
   #CERT_HASH=$(sudo security find-certificate -c "GlobalSign Root CA - R3" -Z /Library/Keychains/System.keychain | \
     #grep "SHA-1 hash:" | awk '{print $3}')
   CERT_HASH=$(openssl x509 -in integration-tests/one-existing-ca.pem -noout -fingerprint -sha1 | cut -d= -f2 | tr -d :)
-
-
   if [ -z "$CERT_HASH" ]; then
     echo "No matching certificate found to delete"
     return
